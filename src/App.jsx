a8ffa5863ff475ee2735ce806eea03e4c17d0230
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-05-01 18:30:03 +0330

    app jsx add

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product"
-import Homepages from "./pages/Homepage"
-import Pricing from "./pages/Pricing"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepages from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
 import Pagenotfound from "./pages/Pagenotfound";
-import Login from "./pages/Login"
-import AppLayout from "./pages/AppLayout"
-import CityList from "../components/CityList";
-import { useEffect, useState } from "react";
-import CountryList from "../components/CountryList";
-import City from "./components/City"
-const fetchUrlLink = "http://localhost:9000"
-export default function App(){
-  const [cities,setCities] = useState([])
-  const [ loading , isLoading ] = useState(false)
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
 
-  useEffect(function(){    
-async function fetchCities (){
-try{
-  isLoading(true)
-  const res = await fetch(`${fetchUrlLink}/cities`)
-  console.log(res);
-  const data = await res.json()
-setCities(data)}
-catch{
-  alert('error')
-}finally{
-  isLoading(false)
-}}
-fetchCities()
- },[])
-
-  return <BrowserRouter>
-  
-  <Routes>
-    <Route path="Login" element={<Login/>}/> 
-    <Route  path="product" element={<Product/>  }/>
-    <Route  path="Pricing" element={<Pricing/>}/>
-    <Route  path="app" element={<AppLayout />}>
-
-      <Route index  element={<CityList cities={cities} isLoading={isLoading}/>} />
-      <Route path="cities/:id" element={<City/>}/>
-      <Route  path="Cities" element={<CityList cities={cities} loading={loading}/> } />
-      <Route path="Countries" element={<CountryList cities={cities} isLoading={isLoading}/>}/>
-      <Route path="Form" element={<p>Form</p>}/>
-
-    </Route>
-    <Route index element={<Homepages/>}/>
-    <Route  path="*" element={<Pagenotfound/>}/>
-  </Routes>
-
-  </BrowserRouter>
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "../src/contexts/CitiesContext";
+export default function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Login" element={<Login />} />
+          <Route path="product" element={<Product />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate to="cities" replace />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="Cities" element={<CityList />} />
+            <Route path="Countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route index element={<Homepages />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
 }
